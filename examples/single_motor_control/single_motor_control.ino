41ca0dbc07d82f3505926374816f6123f36b0a80
HaoYuLiu0725 <lhjh0212531@gmail.com>
2023-09-28 15:42:38 +0800

    Add single_motor_control.ino in examples

---
+++ examples/single_motor_control/single_motor_control.ino
@@ -0,0 +1,36 @@
+#include <LKM_Motor.h>
+
+//create a LKM_Motor object
+LKM_Motor motor1(1, 8, 5); //(id, reduction_ratio, serial_port)
+
+void setup() {
+  
+  motor1.Serial_Init(); 
+  Serial.begin(115200);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(11, HIGH);
+  Serial.println("-> Go to 60 !");
+  motor1.Write_Angle_MultiRound(60, 300);
+  motor1.Print_Data();
+  delay(2000);
+  digitalWrite(11, LOW);
+  Serial.println("-> read ~");
+  motor1.Read_Angle_MultiRound();
+  motor1.Print_Angle();
+  delay(2000);
+
+  digitalWrite(12, HIGH);
+  Serial.println("-> Go to 0 !");
+  motor1.Write_Angle_MultiRound(0, 300);
+  motor1.Print_Data();
+  delay(2000);
+  digitalWrite(12, LOW);
+  Serial.println("-> read ~");
+  motor1.Read_Angle_MultiRound();
+  motor1.Print_Angle();
+  delay(2000);
+}
